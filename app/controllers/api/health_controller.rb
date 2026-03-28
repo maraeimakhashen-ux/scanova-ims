2f974fde7357154426f38b75bbb4a78dce144466
maraeimakhashen-ux <maraei.makhashen@gmail.com>
2026-03-28 20:59:53 +0300

    Remove old api/ controllers (moved to api/ims/)

--- app/controllers/api/health_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module Ims
-    class HealthController < ApplicationController
-      def show
-        render json: { status: "ok", timestamp: Time.current.iso8601 }
-      end
-    end
-  end
-end
